192f98099753f8ed0e79cc349bb456ec097ce7a5
AlexCODE2023 <158850962+AlexCODE2023@users.noreply.github.com>
2024-03-03 20:04:51 +0000

    practica 3

--- modulo3/practica/ej2p3.py
+++ modulo3/practica/ej2p3.py
@@ -1,11 +1,19 @@
+import re
 class producto:
     codigo:str
     nombre:str
 
     def __init__(self,cod,nombre) -> None:
-        self.codigo = cod
+        if re.findall(r"/w+-/d{5}-/d{4}",cod) != []:
+            self.codigo = cod
+        else:
+            self.codigo = "TACNA".upper()+"-00000-"+"2024"
         self.nombre = nombre
 
     def __str__(self) -> str:
         palabras = self.codigo.split(sep="-")
-        return f"se identifico con {palabras[1]} en {palabras[0]}"
+        return f"se identifico {self.nombre} con {palabras[1]}"
+
+productos = []
+productos.append(producto("LIMA-00010-2023","Shampoo"))
+print(productos[0])
